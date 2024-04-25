0316fe162e3466660e6072792306e38952ef3579
Komarov Sergey <onubee@gmail.com>
2024-04-25 13:30:39 +0300

    connect to postgres db

--- src/main.py
+++ src/main.py
@@ -1,13 +1,44 @@
-from fastapi import FastAPI
+from datetime import date
+
+from fastapi import FastAPI, Query, Depends
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class SHotel(BaseModel):
+    address: str
+    name: str
+    stars: int
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+class HotelsSearchParams:
+    def __init__(
+            self,
+            location: str,
+            date_from: date,
+            date_to: date,
+            has_spa: bool | None = None,
+            stars: int | None = Query(None, ge=1, le=5),
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.has_spa = has_spa
+        self.stars = stars
+
+
+@app.get("/hotels/")
+async def say_hello(search_params: HotelsSearchParams = Depends()):
+    return search_params
+
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+@app.post("/bookings")
+def add_booking(booking: SBooking):
+    pass
